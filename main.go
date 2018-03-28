fabbe62a0b72c26096354a5fad2d96fee1693482
Vadim <chuvak117@gmail.com>
2018-03-28 08:39:28 +0300

    Added GUI, update travis-ci

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"github.com/andlabs/ui"
 	"os"
 	"runtime"
-	"time"
 )
 
 func main() {
@@ -13,40 +12,109 @@ func main() {
 
 	respChan := make(chan QR)
 
-	fmt.Printf("Enter path to file: ")
-	var path string
-	fmt.Scan(&path)
+	err := ui.Main(func() {
 
-	fmt.Println("Reading file")
+		var prox []string
+		var uniqueProxies []string
 
-	prox := readFromFile(path)
+		//Creating elements
 
-	uniqueProxies := unique(prox)
+		//Creating input field
+		input := ui.NewEntry()
 
-	fmt.Println("Got", len(uniqueProxies), "unique proxies")
+		//Creating buttons
+		button := ui.NewButton("Open File")
+		button1 := ui.NewButton("Start Checking")
+		button3 := ui.NewButton("Exit")
 
-	time.Sleep(2 * time.Second)
+		//Creating labels
+		greeting := ui.NewLabel("")
+		res := ui.NewLabel("")
 
-	fmt.Println("Starting checking proxies")
+		//Creating progress bar
+		pb := ui.NewProgressBar()
 
-	realIP := getRealIP()
+		//Creating box
+		box := ui.NewVerticalBox()
 
-	for _, proxy := range uniqueProxies {
+		//Appending elements to box
+		box.Append(ui.NewLabel("Path to file with proxies"), false)
+		box.Append(input, false)
+		box.Append(ui.NewLabel("\n"), false)
+		box.Append(button, false)
+		box.Append(button1, false)
+		box.Append(greeting, false)
+		box.Append(ui.NewLabel("Progress"), false)
+		box.Append(pb, false)
+		box.Append(ui.NewLabel("\n"), false)
+		box.Append(res, false)
+		box.Append(ui.NewLabel("\n"), false)
+		box.Append(button3, false)
 
-		go checkProxy(proxy, respChan, realIP)
+		//Creating window
+		window := ui.NewWindow("ProxyChecker", 150, 150, false)
+		window.SetMargined(true)
+		window.SetChild(box)
+
+		button3.Hide()
+
+		//Button click event
+		button.OnClicked(func(*ui.Button) {
+
+			//Open file
+			input.SetText(ui.OpenFile(window))
+
+			prox = readFromFile(input.Text())
+			uniqueProxies = unique(prox)
+		})
+
+		//Button click event
+		button1.OnClicked(func(*ui.Button) {
+
+			//Updating progress bar value
+			pb.SetValue(30)
+
+			realIP := getRealIP()
+			for _, proxy := range uniqueProxies {
+				go checkProxy(proxy, respChan, realIP)
+			}
+
+			//Updating progress bar value
+			pb.SetValue(80)
+
+			os.Create(`live-proxies.txt`)
+			for range uniqueProxies {
+				r := <-respChan
+				if r.Res {
+					writeToFile(r.Addr)
+				}
+			}
+
+			//Updating progress bar value
+			pb.SetValue(100)
+
+			res.SetText("Finish, check your proxies in live-proxies.txt")
+
+			button3.Show()
+
+			//Button click event
+			button3.OnClicked(func(*ui.Button) {
+
+				//Close window if button clicked
+				ui.Quit()
+			})
+		})
+
+		//Event when window closing
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+
+		window.Show()
+	})
+
+	if err != nil {
+		panic(err)
 	}
-
-	os.Create(`live-proxies.txt`)
-
-	fmt.Println("Writing valid proxies to file")
-
-	for range uniqueProxies {
-		r := <-respChan
-
-		if r.Res {
-			writeToFile(r.Addr)
-		}
-	}
-
-	time.Sleep(2 * time.Second)
 }
